ea05632adc4c062225e79c3b305fef3aa10cae8d
Elizabeth Magalhaes <84098531+elizabethmagal@users.noreply.github.com>
2024-11-08 15:26:51 -0300

    Criada a tela de vendas e filtro para listar apenas itens vendidos.

--- src/com/leilao/telas/listagemVIEW.java
+++ src/com/leilao/telas/listagemVIEW.java
@@ -150,6 +150,9 @@ public class listagemVIEW extends javax.swing.JFrame {
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
         //vendasVIEW vendas = new vendasVIEW(); 
         //vendas.setVisible(true);
+       
+         vendaView vendas = new vendaView(); 
+         vendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
